2c3bd4fd74f00e9859ac34a3da9686bb646d8a38
Kenneth Presedo <kennethpresedo@live.com>
2023-03-12 13:31:32 -0400

    duluc learning center initialized

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+//IMPORT BrowserRouter and rename it to Router
+import { BrowserRouter as Router } from "react-router-dom";
+
+const root = createRoot(document.getElementById("root"))
+
+//Wrap the App Component with the Router component to enable the router features
+root.render(
+  <StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </StrictMode>
+);
