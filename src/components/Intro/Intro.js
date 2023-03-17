24937df55f9ca7611b6c7f8863ab3e0191cc2e49
Kenneth Presedo <kennethpresedo@live.com>
2023-03-17 16:35:32 -0400

    intro banner created

---
+++ src/components/Intro/Intro.js
@@ -0,0 +1,13 @@
+import styles from './Intro.module.scss'
+
+export default function Intro(){
+    return(
+        <>
+            <div className={styles.intro}>
+                <p className={styles.missionStatement}>
+                    <p className={styles.mission}>Welcome to Duluc Learning Center!</p><br />Our mission is to provide high quality, individualized tutoring that supports student learning and empowers students to achieve academic success, independence and confidence.
+                </p>
+            </div>
+        </>
+    )
+}
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,13 @@
 import styles from './Home.module.scss'
 import React from "react"
 import NavigationBar from '../../components/NavigationBar/NavigationBar'
+import Intro from '../../components/Intro/Intro'
 
 export default function Home() {
     return(
         <>
         <NavigationBar />
+        <Intro />
         <div className={styles.home}>
             <h1>Home Page!</h1>
         </div>
