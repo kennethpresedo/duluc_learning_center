ca04f06d5c1089fcc353ebd324fb2f1c4847e271
Kenneth Presedo <kennethpresedo@live.com>
2023-03-19 13:44:35 -0400

    back-end added for form and IG api

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 require('dotenv').config()
+require('./config/database');
+const express = require('express')
 const path = require('path')
-app.use(favicon(path.join(__dirname, 'build', 'favicon.ico' )))
+const favicon = require('serve-favicon')
+const logger = require('morgan')
+const PORT = process.env.PORT || 3001
 
-app.use('/')
-app.use('/about')
-app.use('/contact')
-app.use('/services')
+const app = express()
+
+app.use(express.json())// req.body
+app.use((req, res, next) => {
+    res.locals.data = {}
+    next()
+})
+app.use(logger('dev'))
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico' )))
+app.use(express.static(path.join(__dirname, 'build')))
+
+app.get('/api/test', (req, res) => {
+    res.json({'eureka': 'you have found it'})
+})
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+
+app.listen(PORT, () => {
+    console.log(`I am listening on ${PORT}`)
 })
