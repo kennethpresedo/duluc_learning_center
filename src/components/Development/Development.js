9c58a4d5b2c0869013d51a4773bfcd33cd0edc0c
Kenneth Presedo <kennethpresedo@live.com>
2023-04-15 17:59:09 -0400

    fixed service banner

--- src/components/Development/Development.js
+++ src/components/Development/Development.js
@@ -8,9 +8,9 @@ export default function Development() {
 
                     <span className={styles.header}>Why Duluc Learning?</span>
                     <span className={styles.click}>
-                        <span className={styles.ul1}>Our tutors are all certified teachers with a vast knowledge of the curriculum and how to scaffold concepts to help your child understand.</span>
-                        <span className={styles.ul2}>Our tutors have the knowledge and experience to work with all kinds of students including but not limited to those with special needs.</span>
-                        <span className={styles.ul3}>We work with parents and children to create an academic plan that is best for each child.</span>
+                        <span className={styles.ul1}>Our tutors are all certified teachers with a vast knowledge of the curriculum and how to scaffold concepts to help your child understand. </span>
+                        <span className={styles.ul2}>Our tutors have the knowledge and experience to work with all kinds of students including but not limited to those with special needs. </span>
+                        <span className={styles.ul3}>We work with parents and children to create an academic plan that is best for each child. </span>
                         <span className={styles.ul4}>We track students progress consistently and make changes when needed in order for each to succeed.</span>
                     </span>
 
