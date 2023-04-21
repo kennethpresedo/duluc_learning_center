6993253fee8da0d81bc5ae4efcddb8158d2a1cc4
Kenneth Presedo <kennethpresedo@live.com>
2023-04-21 15:38:35 -0400

    routing adjustments

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ require('dotenv').config()
 const path = require('path')
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico' )))
 
-app.use('/')
-app.use('/about')
-app.use('/contact')
-app.use('/services')
+app.use('https://www.duluclearning.com/')
+app.use('https://www.duluclearning.com/about')
+app.use('https://www.duluclearning.com/contact')
+app.use('https://www.duluclearning.com/services')
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
